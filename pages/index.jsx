f5165c3cf9c02d9ef2239c78fc03da910d03cbcd
Herminio Neto <herminio_neto@outlook.com>
2022-08-09 15:55:34 -0300

    Importado fonte e configurado title

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,23 @@
+import Head from "next/head"
+
 import Title from "../src/components/title/Title"
 import Subtitle from "../src/components/subtitle/Subtitle"
 
 function HomePage () {
   // Pode-se usar tanto a tag com fechamento automático quanto a normal:
   return (
-    <div>
-      <Title>Um Título</Title>
-      <Subtitle text="Um subtítulo" />
-      <Title>Outro Título</Title>
-      <Subtitle />
-    </div>
+    <>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      <div>
+        <Title>Um Título</Title>
+        <Subtitle text="Um subtítulo" />
+        <Title>Outro Título</Title>
+        <Subtitle />
+        <p>Um parágrafo qualquer</p>
+      </div>
+    </>
   )
 }
 
